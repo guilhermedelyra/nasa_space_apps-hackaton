ffc5c554ed75a49df782bf5a5306e186051159c0
Guilherme de Lyra P <guilyra12@gmail.com>
2018-10-20 18:43:12 +0000

    som e acelerometro

--- Assets/CollisionGameObject.cs
+++ Assets/CollisionGameObject.cs
@@ -6,8 +6,8 @@ using TMPro;
 public class CollisionGameObject : MonoBehaviour
 {
     public GameObject canvas;
-    public TextMeshProUGUI radiation, member_title;
-    public Image bar_radiation;
+    public TextMeshProUGUI radiation, sound, tempo, member_title;
+    public Image bar_radiation, bar_sound, bar_tempo;
 
     void OnTriggerEnter(Collider collision)
     {
@@ -17,11 +17,22 @@ public class CollisionGameObject : MonoBehaviour
             canvas.SetActive(!canvas.activeSelf);
             if (canvas.activeSelf) {
                 System.Random rnd = new System.Random();
-                int random = rnd.Next(0, 7000);
+                int random = rnd.Next(1000, 7000);
+                int randomT = rnd.Next(40, 60);
+                int randomS = rnd.Next(30, 140);
                 radiation.text = random.ToString() + "sV";
+                sound.text = randomS.ToString() + "db";
+                tempo.text = randomT.ToString() + "s";
+
                 float hue = (255*random)/7000;
+                float hue1 = (255*randomS)/140;
+                float hu2 = (255*randomT)/60;
+                bar_sound.fillAmount = hue1/255;
+                bar_tempo.fillAmount = hu2/255;
                 bar_radiation.fillAmount = hue/255;
+                
                 radiation.color = new Color32((byte)(hue), (byte)(255 - hue), 0, 255);
+                sound.color = new Color32((byte)(hue), (byte)(255 - hue1), 0, 255);
                 member_title.text = char.ToUpper(gameObject.name.Split('_')[1][0])+gameObject.name.Split('_')[1].Substring(1);
             }
         }
